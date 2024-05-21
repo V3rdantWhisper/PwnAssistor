2a679871a5f1337fcab2f4a165c9e46c8b93e3e1
nemo <wzhdxtx123@outlook.com>
2024-05-22 00:58:33 +0800

    update workflows

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="PwnAssistor",
-    version="0.2.2",
+    version="0.2.3",
     description="Atool for pwn",
     author="V3rdant",
     long_description=open('README.md').read(),
