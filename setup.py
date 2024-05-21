310628044c91a49390a5dda736bcfdaf44b36e55
nemo <wzhdxtx123@outlook.com>
2024-05-22 00:45:10 +0800

    update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="PwnAssistor",
-    version="0.2.1",
+    version="0.2.2",
     description="Atool for pwn",
     author="V3rdant",
     long_description=open('README.md').read(),
